887f3c192cb89f034b0ade7a5fd2583fe3d63c76
Jan-Luca Splettstößer <jan-luca@spletti-din.de>
2022-07-07 09:17:11 +0200

    Modify project structure and create a vite config file

--- src/script/main.js
+++
@@ -1,4 +0,0 @@
-import '/src/styles/main.scss'
-import demoPlugin from './plugins/demo.plugin'
-
-const demo = new demoPlugin('#app')
--- src/script/plugins/demo.plugin.js
+++
@@ -1,7 +0,0 @@
-export default class demoPlugin {
-    constructor(el) {
-        this.el = document.querySelector(el);
-
-        console.log(this.el);
-    }
-}
